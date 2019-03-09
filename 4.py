511bff7402f6df2ac4551ba3e37144509072c8b0
Peshal Oli <peshal@peshal.com.np>
2019-03-08 19:08:18 -0500

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,15 @@
+
+https://colab.research.google.com/drive/1MrrazjWtzSLCW_HZIDjzRdck6wG513ZN
+
+Git:
+
+https://www.youtube.com/watch?v=SWYqp7iY_Tc
+
+git config --global user.email 'peshal@peshal.com.np'
+git config --global user.name 'peshal2404’'
+
+git rm --cached  3.py
+Git add .
+Git add *.html
+
+Touch .gitignore
